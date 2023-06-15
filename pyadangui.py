39b4950208cba102fca31d0c3d3d4378b8bc54e5
CyberBow <rizksalimrizk@gmail.com>
2023-06-16 01:56:54 +0300

    add img

--- pyadangui.py
+++ pyadangui.py
@@ -50,10 +50,12 @@ esha_time = get_data(esha_class)
 mid_night_time = get_data(mid_night_class)
 
 # image variables
-morning = CTkImage(Image.open(r"images/morning.jpg"))
-midday = CTkImage(Image.open(r"images/midday.jpg"))
-evening = CTkImage(Image.open(r"images/evening.jpg"))
-midnight = CTkImage(Image.open(r"images/midnight.jpg"))
+morning_img = CTkImage(Image.open(r"images/morning.jpg"))
+midday_img = CTkImage(Image.open(r"images/midday.jpg"))
+evening_img = CTkImage(Image.open(r"images/evening.jpg"))
+midnight_img = CTkImage(Image.open(r"images/midnight.jpg"))
+refresh_img = CTkImage(Image.open(r"images/refresh.png"))
+
 
 def refresh_clicked():
     pass
@@ -68,36 +70,44 @@ class MyFrame(CTkFrame):
         self.label_imsak_arb = CTkLabel(self, text=imsak_arb)
         self.label_fajr_time = CTkLabel(self, text=fajr_time)
         self.label_fajr_arb = CTkLabel(self, text=fajr_arb)
+        self.image_morning_pray = CTkLabel(self, text="", image=morning_img)
         self.label_shorok_time = CTkLabel(self, text=shorok_time)
         self.label_shorok_arb = CTkLabel(self, text=shorok_arb)
         self.label_dohor_time = CTkLabel(self, text=dohor_time)
         self.label_dohor_arb = CTkLabel(self, text=dohor_arb)
+        self.image_dohor_pray = CTkLabel(self, text="", image=midday_img)
         self.label_aser_time = CTkLabel(self, text=aser_time)
         self.label_aser_arb = CTkLabel(self, text=aser_arb)
         self.label_mogreb_time = CTkLabel(self, text=mogreb_time)
         self.label_mogreb_arb = CTkLabel(self, text=mogreb_arb)
+        self.image_mogreb_pray = CTkLabel(self, text="", image=evening_img)
         self.label_esha_time = CTkLabel(self, text=esha_time)
         self.label_esha_arb = CTkLabel(self, text=esha_arb)
         self.label_mid_night_time = CTkLabel(self, text=mid_night_time)
         self.label_mid_night_arb = CTkLabel(self, text=mid_night_arb)
+        self.image_mid_night_pray = CTkLabel(self, text="", image=midnight_img)
 
         self.label_date_arb.grid(row=0, column=0, padx=30, pady=10, columnspan=2)
         self.label_imsak_time.grid(row=1, column=0, padx=30, pady=10)
         self.label_imsak_arb.grid(row=1, column=1, padx=30, pady=10)
         self.label_fajr_time.grid(row=2, column=0, padx=30, pady=10)
         self.label_fajr_arb.grid(row=2, column=1, padx=30, pady=10)
+        self.image_morning_pray.grid(row=2, column=2, padx=10)
         self.label_shorok_time.grid(row=3, column=0, padx=30, pady=10)
         self.label_shorok_arb.grid(row=3, column=1, padx=30, pady=10)
         self.label_dohor_time.grid(row=4, column=0, padx=30, pady=10)
         self.label_dohor_arb.grid(row=4, column=1, padx=30, pady=10)
+        self.image_dohor_pray.grid(row=4, column=2, padx=10)
         self.label_aser_time.grid(row=5, column=0, padx=30, pady=10)
         self.label_aser_arb.grid(row=5, column=1, padx=30, pady=10)
         self.label_mogreb_time.grid(row=6, column=0, padx=30, pady=10)
         self.label_mogreb_arb.grid(row=6, column=1, padx=30, pady=10)
+        self.image_mogreb_pray.grid(row=6, column=2, padx=10)
         self.label_esha_time.grid(row=7, column=0, padx=30, pady=10)
         self.label_esha_arb.grid(row=7, column=1, padx=30, pady=10)
         self.label_mid_night_time.grid(row=8, column=0, padx=30, pady=10)
         self.label_mid_night_arb.grid(row=8, column=1, padx=30, pady=10)
+        self.image_mid_night_pray.grid(row=8, column=2, padx=10)
 
 
 class App(CTk):
@@ -106,7 +116,7 @@ class App(CTk):
         self.title("PyAdan")
         self.grid_columnconfigure(0, weight=1)
 
-        self.refresh_button = CTkButton(self, command=refresh_clicked())
+        self.refresh_button = CTkButton(self, command=refresh_clicked(), image=refresh_img)
         self.refresh_button.pack(pady=10)
 
         self.my_frame = MyFrame(master=self)

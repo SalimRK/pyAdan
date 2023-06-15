550be6bb67e85e2cb4abf10635e61b8b04cdfec9
CyberBow <rizksalimrizk@gmail.com>
2023-06-15 19:01:25 +0300

    scraping data from almaaref.org.lb

--- pyadan.py
+++ pyadan.py
@@ -7,12 +7,24 @@ response = requests.get("https://almaaref.org.lb/")
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the container that holds the prayer times
-container = soup.find("div", class_="card h20 mb-3")
+date_class = "fw-bold"
+emsak_class = "card mx-auto bg-warning text-light mb-1"
+fajer_class = "card mx-auto bg-success text-light mb-1"
+shorok_calss = "card mx-auto bg-info text-light mb-1"
+dohor_class = "card mx-auto bg-primary text-light mb-1"
+aser_class = "card mx-auto bg-danger text-light mb-1"
 
-# Extract the prayer times
-prayer_times = container.find_all("td")
+def get_data(html_class):
+    # Find the element with the specific class containing the date information
+    data_element = soup.find(class_=html_class)  # Replace "your-class" with the appropriate class value
+    # Extract the date text
+    data_text = data_element.get_text(strip=True)
+    return data_text
 
-# Print the prayer times
-for prayer in prayer_times:
-    print(prayer.get_text(strip=True))
+
+date_arb = f" مواقيــت الصـــلاة ليوم {get_data(date_class)} حسب توقيت مدينة بيروت "
+emsak_arb = "الإمساك"
+fajer_arb =  "الفجر"
+shorok_arb = "الشروق"
+dohor_arb = "الظهر"
+aser_arb = "العصر"
